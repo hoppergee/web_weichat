bb81db511704e49df7734ed5b6fda3b60cf982c6
Ji Xiang <jx4554@gmail.com>
2017-07-09 03:23:15 +0800

    beauty sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,20 @@ module ApplicationHelper
 			"active"
 		end
 	end
+
+	def image_when_current_in_chatrooms
+		if params[:controller] == "chatrooms"
+			image_tag "Tabbar-Chat-Selected", size: "22x20"
+		else
+			image_tag "Tabbar-Chat", size: "22x20"
+		end
+	end
+
+	def image_when_current_in_contacts
+		if params[:controller] == "contacts"
+			image_tag "Tabbar-Contacts-Selected", size: "22x20"
+		else
+			image_tag "Tabbar-Contacts", size: "22x20"
+		end
+	end
 end
