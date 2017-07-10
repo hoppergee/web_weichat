e0216b1a5ce52d12783d6b39523be9613b19dbb6
Ji Xiang <jx4554@gmail.com>
2017-07-10 19:11:43 +0800

    chatroom can show correct name

--- app/helpers/chatrooms_helper.rb
+++ app/helpers/chatrooms_helper.rb
@@ -1,2 +1,9 @@
 module ChatroomsHelper
+	def render_name_of(chatroom)
+		if chatroom.private
+			chatroom.users.where.not(id: current_user.id).first.username
+		else
+			chatroom.name.blank? ? "#{chatroom.users[0..2].map{|u| u.username}.join(',')}..." : chatroom.name
+		end
+	end
 end
